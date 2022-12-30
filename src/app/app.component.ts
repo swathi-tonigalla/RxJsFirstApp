8a802e9fdc698d4f67a52b40fe2276253f9b829d
swathi-tonigalla <120887962+swathi-tonigalla@users.noreply.github.com>
2022-12-30 21:28:50 +0530

    With error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, OnInit, VERSION } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
+
+
+
+export class AppComponent implements OnInit  {
+ngOnInit(): void {
+  const Observer = {
+    next: apple => console.log(`emitted ${apple}`),
+    error: err => console.log(`error occured ${err}`),
+    complete: () =>console.log('completed')
+        
+    };
+    const apple$ = new Observable(appSubscriber =>
+      {
+        appSubscriber.next('Apple1')
+        appSubscriber.next('Apple2')
+        appSubscriber.complete();
+      });
+  let sub = apple$.subscribe(Observer);
+}
   name = 'Angular ' + VERSION.major;
+
+  
 }
